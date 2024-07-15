1c26ed78ffa2ff188a55dd62e5f25539fad115b5
dotsenko <igor.dots@gmail.com>
2024-07-15 19:30:36 +0300

    Test

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="manifest" href="../../manifest.json" />
+
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-starturl" content="/" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta name="theme-color" content="#e5e5e5" />
+
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
